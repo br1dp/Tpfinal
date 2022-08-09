328e61fa9e4afd5d7428f86eb36175bb9deb535a
Aymara <aymaraflaschka@gmail.com>
2022-08-09 11:08:13 -0300

    solo agregué botones para redireccionar cuando no haya resultados

--- EscaladaProject/app_1/urls.py
+++ EscaladaProject/app_1/urls.py
@@ -15,14 +15,16 @@ from app_1.views import inicio
 urlpatterns = [
     path('',inicio,name = 'Inicio'),
     path('cursos/',cursos,name = 'Cursos'),
-    path('alumnos/',alumnos,name = 'Alumnos'),
-    path('profesores/',profesores,name = 'Profesores'),
     path('cursoformulario/',cursoformulario,name = 'CursoFormulario'),
     path('busquedacursos/',busquedacursos,name = 'BuscarCursos'),
     path('resultadocursos/',resultadocursos,name = 'ResultadoCursos'),
+
+    path('alumnos/',alumnos,name = 'Alumnos'),
     path('alumnosformulario/',alumnosformulario,name = 'AlumnosFormulario'),
     path('busquedaalumnos/',busquedaalumnos,name = 'BusquedaAlumnos'),
     path('resultadoalumnos/',resultadoalumnos,name = 'ResultadoAlumnos'),
+    
+    path('profesores/',profesores,name = 'Profesores'),
     path('profesoresformulario/',profesoresformulario,name = 'ProfesoresFormulario'),
     path('busquedaprofesores/',busquedaprofesores,name = 'BusquedaProfesores'),
     path('resultadoprofesores/',resultadoprofesores,name = 'ResultadoProfesores'),
